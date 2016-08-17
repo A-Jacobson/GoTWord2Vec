3cff85cb09b89cc1672ef23f565482a3a5e32bc3
Austin <JacobsonAustinJ@gmail.com>
2016-08-17 05:23:28 -0400

    further preprocessing on corpus, ad bigram transformer, remove lowercase

--- word2vec.py
+++ word2vec.py
@@ -1,4 +1,4 @@
-from gensim.models import word2vec
+from gensim.models import Word2Vec, Phrases
 from nltk.tokenize import sent_tokenize, word_tokenize
 import re
 import logging
@@ -7,7 +7,7 @@ import os
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-text = open("data/corpus.txt").read().lower()
+text = open("data/corpus.txt").read()
 
 # remove newline characters
 text = re.sub('\n+', "", text)
@@ -15,8 +15,8 @@ text = re.sub('\n+', "", text)
 # Word2Vec accepts sequences of sentence so we will use nltk's punkt tokenizer to split our corpus into sentences
 sentences = sent_tokenize(text)
 sentences = [word_tokenize(s) for s in sentences]
-print sentences[0]
 
+bigram_transformer = Phrases(sentences)
 
-model = gensim.models.Word2Vec(sentences, min_count=3, size=100, workers=4)
+model = Word2Vec(bigram_transformer[sentences], min_count=5, size=100, workers=4)
 model.save(os.path.join("models", "GoT"))
