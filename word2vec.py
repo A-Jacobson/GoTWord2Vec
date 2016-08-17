b0e840c88b68a65416fea1683569f92d464c4d4e
Austin <jacobsonaustinj@gmail.com>
2016-08-17 03:21:56 -0400

    trained model

--- word2vec.py
+++ word2vec.py
@@ -1,14 +1,22 @@
-# from gensim.models import word2vec
-from nltk.tokenize import sent_tokenize
+from gensim.models import word2vec
+from nltk.tokenize import sent_tokenize, word_tokenize
 import re
 import logging
 import gensim
+import os
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 text = open("data/corpus.txt").read().lower()
+
+# remove newline characters
 text = re.sub('\n+', "", text)
 
+# Word2Vec accepts sequences of sentence so we will use nltk's punkt tokenizer to split our corpus into sentences
 sentences = sent_tokenize(text)
+sentences = [word_tokenize(s) for s in sentences]
+print sentences[0]
 
-model = gensim.models.Word2Vec(sentences, min_count=1, size=100, workers=4)
+
+model = gensim.models.Word2Vec(sentences, min_count=3, size=100, workers=4)
+model.save(os.path.join("models", "GoT"))
