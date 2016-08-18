4289f92d178a167576f6f798396c43e4a6fc3cd0
Austin <JacobsonAustinJ@gmail.com>
2016-08-18 03:17:22 -0400

    pca, t-sne

--- word2vec.py
+++ word2vec.py
@@ -4,19 +4,24 @@ import re
 import logging
 import gensim
 import os
+import string
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-text = open("data/corpus.txt").read()
+text = open("data/corpus.txt").read().lower()
 
 # remove newline characters
 text = re.sub('\n+', "", text)
+# remove punctuation
+def remove_punctuation(s):
+    return s.translate(string.maketrans("",""), string.punctuation)
+
 
 # Word2Vec accepts sequences of sentence so we will use nltk's punkt tokenizer to split our corpus into sentences
 sentences = sent_tokenize(text)
-sentences = [word_tokenize(s) for s in sentences]
+sentences = [word_tokenize(remove_punctuation(s)) for s in sentences]
 
-bigram_transformer = Phrases(sentences)
+# bigram_transformer = Phrases(sentences)
 
-model = Word2Vec(bigram_transformer[sentences], min_count=5, size=100, workers=4)
+model = Word2Vec(sentences, min_count=5, size=100, workers=4)
 model.save(os.path.join("models", "GoT"))
